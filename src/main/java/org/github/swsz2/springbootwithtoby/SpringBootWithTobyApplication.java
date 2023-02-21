ebd860beb4c77a05291a80953456da0a2ab598e8
sungwonkim-muhayu <sungwonkim@muhayu.com>
2023-02-21 20:13:39 +0900

    [init] 프로젝트 생성

---
+++ src/main/java/org/github/swsz2/springbootwithtoby/SpringBootWithTobyApplication.java
@@ -0,0 +1,13 @@
+package org.github.swsz2.springbootwithtoby;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootWithTobyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootWithTobyApplication.class, args);
+    }
+
+}
