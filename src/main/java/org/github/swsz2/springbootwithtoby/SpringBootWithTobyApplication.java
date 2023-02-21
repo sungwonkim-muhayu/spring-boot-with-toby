769555a8caff1b1749b8f1957888e781531b134f
sungwonkim-muhayu <sungwonkim@muhayu.com>
2023-02-21 20:31:24 +0900

    [feat] HelloController 추가

--- src/main/java/org/github/swsz2/springbootwithtoby/SpringBootWithTobyApplication.java
+++ src/main/java/org/github/swsz2/springbootwithtoby/SpringBootWithTobyApplication.java
@@ -6,8 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class SpringBootWithTobyApplication {
 
-    public static void main(String[] args) {
-        SpringApplication.run(SpringBootWithTobyApplication.class, args);
-    }
-
+  public static void main(String[] args) {
+    SpringApplication.run(SpringBootWithTobyApplication.class, args);
+  }
 }
