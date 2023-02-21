e468d168d4d7e092d36269e6cc8ad2642f8a6f81
sungwonkim-muhayu <sungwonkim@muhayu.com>
2023-02-21 21:57:09 +0900

    [feat] 비즈니스 로직을 SimpleHelloService로 위임

--- src/main/java/org/github/swsz2/springbootwithtoby/controller/HelloController.java
+++ src/main/java/org/github/swsz2/springbootwithtoby/controller/HelloController.java
@@ -1,8 +1,13 @@
 package org.github.swsz2.springbootwithtoby.controller;
 
+import org.github.swsz2.springbootwithtoby.service.SimpleHelloService;
+
+import java.util.Objects;
+
 // @RestController // html 통째로 리턴, api 요청에 대한 응답을 특정 타입으로 인코딩해 전달
 public class HelloController {
   public String hello(String name) {
-    return "hello " + name;
+    final SimpleHelloService helloService = new SimpleHelloService();
+    return helloService.sayHello(Objects.requireNonNull(name));
   }
 }
---
+++ src/main/java/org/github/swsz2/springbootwithtoby/service/SimpleHelloService.java
@@ -0,0 +1,7 @@
+package org.github.swsz2.springbootwithtoby.service;
+
+public class SimpleHelloService {
+  public String sayHello(final String name) {
+    return "hello " + name;
+  }
+}
