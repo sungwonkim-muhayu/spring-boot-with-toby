89154b069998d485872a8d748bd7ab4a300fa2ce
sungwonkim-muhayu <sungwonkim@muhayu.com>
2023-02-21 20:44:00 +0900

    [chore] 스프링 부트 어노테이션 및 run 메소드 제거

--- src/main/java/org/github/swsz2/springbootwithtoby/SpringBootWithTobyApplication.java
+++ src/main/java/org/github/swsz2/springbootwithtoby/SpringBootWithTobyApplication.java
@@ -1,12 +1,6 @@
 package org.github.swsz2.springbootwithtoby;
 
-import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
-
-@SpringBootApplication
 public class SpringBootWithTobyApplication {
 
-  public static void main(String[] args) {
-    SpringApplication.run(SpringBootWithTobyApplication.class, args);
-  }
+  public static void main(String[] args) {}
 }
