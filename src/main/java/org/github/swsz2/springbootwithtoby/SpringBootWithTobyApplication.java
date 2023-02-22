f0790926e0ef3ab05313f129b93099908704de9b
sungwonkim-muhayu <sungwonkim@muhayu.com>
2023-02-22 21:38:54 +0900

    [feat] 자바 코드 기반 빈 등록에서 Annotation 기반 빈 등록 기능으로 변경

--- src/main/java/org/github/swsz2/springbootwithtoby/SpringBootWithTobyApplication.java
+++ src/main/java/org/github/swsz2/springbootwithtoby/SpringBootWithTobyApplication.java
@@ -1,20 +1,35 @@
 package org.github.swsz2.springbootwithtoby;
 
 import org.github.swsz2.springbootwithtoby.controller.HelloController;
+import org.github.swsz2.springbootwithtoby.service.HelloService;
 import org.github.swsz2.springbootwithtoby.service.SimpleHelloService;
 import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
 import org.springframework.boot.web.server.WebServer;
 import org.springframework.boot.web.servlet.server.ServletWebServerFactory;
-import org.springframework.web.context.support.GenericWebApplicationContext;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.context.support.AnnotationConfigWebApplicationContext;
 import org.springframework.web.servlet.DispatcherServlet;
 
+@Configuration // 스프링 컨테이너야. 구성 정보를 가지고 있는 클래스니까, 안에 설정된 빈 오브젝트를 만들어줘!
 public class SpringBootWithTobyApplication {
+
+  @Bean
+  // 스프링 컨테이너야. 내가 헬로 컨트롤러 만들 건데 그때 필요한 의존성을 알아서 주입해줘
+  public HelloController helloController(HelloService helloService) {
+    return new HelloController(helloService);
+  }
+
+  @Bean
+  public HelloService helloService() {
+    return new SimpleHelloService();
+  }
 
   public static void main(String[] args) {
 
     // 서블릿 컨테이너 초기화를 onRefresh 안에서 수행
-    final GenericWebApplicationContext applicationContext =
-        new GenericWebApplicationContext() {
+    final AnnotationConfigWebApplicationContext applicationContext =
+        new AnnotationConfigWebApplicationContext() {
           @Override
           protected void onRefresh() {
             super.onRefresh();
@@ -31,9 +46,12 @@ public class SpringBootWithTobyApplication {
             webServer.start();
           }
         };
-    // 스프링 컨테이너에 빈 등록
-    applicationContext.registerBean(HelloController.class);
-    applicationContext.registerBean(SimpleHelloService.class);
+    // 스프링 컨테이너에 빈 등록 (GenericWebApplicationContext)
+    // applicationContext.registerBean(HelloController.class);
+    // applicationContext.registerBean(SimpleHelloService.class);
+
+    // 어노테이션 기반 빈 등록 (AnnotationConfigWebApplicationContext)
+    applicationContext.register(SpringBootWithTobyApplication.class);
     applicationContext.refresh();
   }
 }
