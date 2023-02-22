dca12227bce4b7d2f18aee37f3a3419aaba5eccb
sungwonkim-muhayu <sungwonkim@muhayu.com>
2023-02-22 21:25:36 +0900

    [feat] 서블릿 컨테이너 초기화를 GenericWebApplicationContext.onRefresh 안에서 수행

--- src/main/java/org/github/swsz2/springbootwithtoby/SpringBootWithTobyApplication.java
+++ src/main/java/org/github/swsz2/springbootwithtoby/SpringBootWithTobyApplication.java
@@ -11,22 +11,29 @@ import org.springframework.web.servlet.DispatcherServlet;
 public class SpringBootWithTobyApplication {
 
   public static void main(String[] args) {
+
+    // 서블릿 컨테이너 초기화를 onRefresh 안에서 수행
+    final GenericWebApplicationContext applicationContext =
+        new GenericWebApplicationContext() {
+          @Override
+          protected void onRefresh() {
+            super.onRefresh();
+            // 빈 서블릿 컨테이너 생성
+            // 서블릿 컨테이너는 어떠한 서블릿이 요청을 처리할지 매핑하는 역할을 가짐
+            final ServletWebServerFactory serverFactory = new TomcatServletWebServerFactory();
+            final WebServer webServer =
+                serverFactory.getWebServer(
+                    servletContext ->
+                        servletContext
+                            // 프론트 컨트롤러의 역할을 디스패처 서블릿이 대신 처리할 수 있도록 함
+                            .addServlet("dispatcherServlet", new DispatcherServlet(this))
+                            .addMapping("/*"));
+            webServer.start();
+          }
+        };
     // 스프링 컨테이너에 빈 등록
-    final GenericWebApplicationContext applicationContext = new GenericWebApplicationContext();
     applicationContext.registerBean(HelloController.class);
     applicationContext.registerBean(SimpleHelloService.class);
     applicationContext.refresh();
-
-    // 빈 서블릿 컨테이너 생성
-    // 서블릿 컨테이너는 어떠한 서블릿이 요청을 처리할지 매핑하는 역할을 가짐
-    final ServletWebServerFactory serverFactory = new TomcatServletWebServerFactory();
-    final WebServer webServer =
-        serverFactory.getWebServer(
-            servletContext ->
-                servletContext
-                    // 프론트 컨트롤러의 역할을 디스패처 서블릿이 대신 처리할 수 있도록 함
-                    .addServlet("dispatcherServlet", new DispatcherServlet(applicationContext))
-                    .addMapping("/*"));
-    webServer.start();
   }
 }
